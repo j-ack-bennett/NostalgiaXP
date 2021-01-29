aa60bdfef240b95d7025e617508dc1adba23540a
Dylan Hodges <73857570+dylan-hodges313@users.noreply.github.com>
2021-01-29 13:36:39 +1300

    add error iamage

--- client/components/Images.jsx
+++ client/components/Images.jsx
@@ -21,20 +21,21 @@ const Images = () => {
 	//note that audio1 etc. must be in numerical order (inside return) to work
 	return (
 		<>
-			<img className='taskbar' src='/taskbar.png' />
+            <img className='taskbar' src='/taskbar.png' />
 			<div className='dialup' onClick={() => audio1.play()}></div>
 			<div className='startup' onClick={() => audio2.play()}></div>
 			<img className='clippy' onClick={() => audio3.play()} src='/clippy.png' />
 			<div className='iconsFlexbox'>
-				<img className='bin' src='/bin.png' />
+				{/* <img className='bin' src='/bin.png' />
 				<img className='notepad' src='/notepad1.png' />
 				<img className='search' src='/search.png' />
-				<img className='settings' src='/settings.png' />
+            <img className='settings' src='/settings.png' /> */}
 				<img className='bin' onClick={() => audio4.play()} src='/bin.png' />
 				<img className='notepad' onClick={() => audio5.play()} src='/notepad1.png' />
 				<img className='search' onClick={() => audio6.play()} src='/search.png' />
 				<img className='settings' onClick={() => audio7.play()} src='/settings.png' />
 			</div>
+            <img className='errorBox' src='/error.png' />
 		</>
 	)
 }
