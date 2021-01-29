4ea1cae131cfa8e9589b7c34aa81839a3ecffdf6
Dylan Hodges <73857570+dylan-hodges313@users.noreply.github.com>
2021-01-29 14:43:41 +1300

    thanks bren

--- client/components/Images.jsx
+++ client/components/Images.jsx
@@ -14,6 +14,7 @@ import exclamation from '../../server/public/exclamation.mp3'
 import chimes from '../../server/public/chimes.mp3'
 import aol from '../../server/public/aol.mp3'
 
+
 const Images = () => {
 
 	let audio1 = new Audio(dialup)
@@ -34,26 +35,31 @@ const Images = () => {
 	//note that audio1 etc. must be in numerical order (inside return) to work
 	return (
 		<>
-      <img className='taskbar' src='/taskbar.png' />
-			<div className='dialup' onClick={() => audio1.play()}></div>
-			<div className='startup' onClick={() => audio2.play()}></div>
-			<div className='msn' onClick={() => audio9.play()}></div>
-			<div className='nineFive' onClick={() => audio10.play()}></div>
-			<div className='chord' onClick={() => audio11.play()}></div>
-			<div className='exclamation' onClick={() => audio12.play()}></div>
-			<div className='chimes' onClick={() => audio13.play()}></div>
-			<div className='aol' onClick={() => audio14.play()}></div>
-			<img className='clippy' onClick={() => audio3.play()} src='/clippy.png' />
-			<div className='iconsFlexbox'>
-				<img className='bin' onClick={() => audio4.play()} src='/bin.png' />
-				<img className='notepad' onClick={() => audio5.play()} src='/notepad1.png' />
-				<img className='search' onClick={() => audio6.play()} src='/search.png' />
-				<img className='settings' onClick={() => audio7.play()} src='/settings.png' />
-			</div>
-		<div className='link'>
-		  <a href='https://geekprank.com/xp-simulator.php'>O</a> 
-		</div>
-            <img className='errorBox' onClick={() => audio8.play()} src='/error.png' />
+            <div className='iconsFlexbox'>
+                <img className='icon' onClick={() => audio4.play()} src='/bin.png' />
+                <img className='icon' onClick={() => audio5.play()} src='/notepad1.png' />
+                <img className='icon' onClick={() => audio6.play()} src='/search.png' />
+                <img className='icon' onClick={() => audio7.play()} src='/settings.png' />
+            </div>
+            <div className='taskbar'>
+                <div className="system-tray"></div>
+                <div className='dialup' onClick={() => audio1.play()}></div>
+                <div className='startup' onClick={() => audio2.play()}></div>
+                <div className='msn' onClick={() => audio9.play()}></div>
+                <div className='nineFive' onClick={() => audio10.play()}></div>
+                <div className='chord' onClick={() => audio11.play()}></div>
+                <div className='exclamation' onClick={() => audio12.play()}></div>
+                <div className='chimes' onClick={() => audio13.play()}></div>
+                <div className='aol' onClick={() => audio14.play()}></div>
+                <img className='clippy' onClick={() => audio3.play()} src='/clippy.png' />
+                
+                <div className='link'>
+                <a href='https://geekprank.com/xp-simulator.php'>O</a> 
+                </div>
+                <img className='errorBox' onClick={() => audio8.play()} src='/error.png' />
+            </div>
+            {/* <img className='taskbar' src='/taskbar.png' /> */}
+			
 		</>
 	)
 }
