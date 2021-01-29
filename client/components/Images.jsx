d9573497176f6c0ed9e50c8136ee70319a0e7d81
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-01-29 13:18:37 +1300

    added more audio to more icons

--- client/components/Images.jsx
+++ client/components/Images.jsx
@@ -1,33 +1,35 @@
 import React from 'react'
 import dialup from '../../server/public/dialup.mp3'
 import startup from '../../server/public/xpstartup.mp3'
+import bin from '../../server/public/recycle.mp3'
+import tada from '../../server/public/tada.mp3'
+import ding from '../../server/public/ding.mp3'
+// import bin from '../../server/public/recycle.mp3'
+// import tada from '../../server/public/tada.mp3'
 
 const Images = () => {
 
 	let audio1 = new Audio(dialup)
 	let audio2 = new Audio(startup)
+	let audio3 = new Audio(tada)
+	let audio4 = new Audio(bin)
+	let audio5 = new Audio(ding)
+	// let audio6 = new Audio(search)
+	// let audio7 = new Audio(settings)
 
-	// const start1 = () => {
-	// 	audio1.play()
-	// }
 
-	// const start2 = () => (
-	// 	audio2.play()
-	// )
-
+	//note that audio1 etc. must be in numerical order (inside return) to work
 	return (
 		<>
-			<div className='dialup' onClick={() => audio1.play()}>
-				<img className='taskbar' src='/taskbar.png' />
-			</div>
-		<div className='startup' onClick={() => audio2.play()}></div>
-			<img className='clippy' src='/clippy.png' />
+			<img className='taskbar' src='/taskbar.png' />
+			<div className='dialup' onClick={() => audio1.play()}></div>
+			<div className='startup' onClick={() => audio2.play()}></div>
+			<img className='clippy' onClick={() => audio3.play()} src='/clippy.png' />
 			<div className='iconsFlexbox'>
-				<img className='bin' src='/bin.png' />
-				<img className='notepad' src='/notepad1.png' />
-				{/* <img className='phone' src='/phone.png' /> */}
-				<img className='search' src='/search.png' />
-				<img className='settings' src='/settings.png' />
+				<img className='bin' onClick={() => audio4.play()} src='/bin.png' />
+				<img className='notepad' onClick={() => audio5.play()} src='/notepad1.png' />
+				<img className='search' onClick={() => audio6.play()} src='/search.png' />
+				<img className='settings' onClick={() => audio7.play()} src='/settings.png' />
 			</div>
 		</>
 	)
